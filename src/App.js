005966c62f9c0f26bee129cf71861f503a0cad80
Georgi <figoto660@gmail.com>
2022-11-16 00:16:39 -0700

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello Test!</h1>
+      <h1><Header /></h1>
+      <Container>
+        <div>
+          <List />
+        </div>
+        <div>
+          <Map />
+        </div>
+      </Container>
     </div>
   );
 }
+
+
+const Container = styled.div`
+display:flex;
+`;
+
 
 export default App;
