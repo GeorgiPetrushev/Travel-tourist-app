4c5cd924111969bb8ae34b3d546ce041129368ae
Georgi <figoto660@gmail.com>
2022-11-19 00:23:30 -0500

    style add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import styled from "styled-components";
+import { BiSearchAlt } from "react-icons/bi";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <StyleHeader>
+      <div>Travel Advisor</div>
+      <div className="header-right">
+        {" "}
+        <h2>Explore New Places</h2>
+        <BiSearchAlt className="search-icon" />
+        <input placeholder="Search for place ..." className="header-input"></input>
+      </div>
+    </StyleHeader>
+  );
+};
 
-export default Header
+const StyleHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  .header-right {
+    display: flex;
+  }
+  .search-icon {
+    margin-left:1rem;
+      font-size: 2rem;
+      height: auto;
+    }
+    .header-input{
+      border:none;
+      font-size:1.4rem;
+      padding:0.5rem;
+      &:focus{
+        outline: none;
+      }
+    }
+`;
+
+export default Header;
