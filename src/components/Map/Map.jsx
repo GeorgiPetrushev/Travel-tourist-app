bc5c9aa24ac8c851829918bb30f5be650a07f94c
Georgi <figoto660@gmail.com>
2022-11-20 21:13:36 -0500

    .

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import GoogleMapReact from "google-map-react";
+
+import styled from "styled-components";
 
 const Map = () => {
-  return (
-    <div>Map</div>
-  )
-}
 
-export default Map
+
+  const coordinates = { lat: 0 , lng: 0 }
+
+  return (
+    <StyleMap>
+      <div>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={coordinates}
+        center = {coordinates}
+        defaultZoom={15}
+        margin={[50,50,50,50]}
+        options = {``}
+        onChange = {``}
+        onChildClick= {``}
+      >
+
+      </GoogleMapReact>
+      </div>
+    </StyleMap>
+  );
+};
+
+const StyleMap = styled.div``;
+
+export default Map;
